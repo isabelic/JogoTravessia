72360c1a65f418c31c2acaf732cb268f8320e453
isabelic <isabeli_ferreira@estudante.sesisenai.org.br>
2025-07-17 16:52:32 -0300

    3 - commit

--- src/classes/Processo.java
+++ src/classes/Processo.java
@@ -9,4 +9,24 @@ public class Processo {
     private List<Personagem> margemD = new ArrayList<>();
     private List<Personagem> margemE = new ArrayList<>();
     private List<Personagem> barco = new ArrayList<>();
+
+
+    public Processo() {
+        margemE.add(new Lobo());
+        margemE.add(new Ovelha());
+        margemE.add(new Repolho());
+        margemE.add(new Pastor());
+    }
+
+    boolean esquerda = true;
+
+    public void Embarcacao(int op) {
+
+        List<Personagem> inicioTravessia;
+        if (esquerda) {
+            inicioTravessia = margemE;
+        } else {
+            inicioTravessia = margemD;
+        }
+    }
 }
